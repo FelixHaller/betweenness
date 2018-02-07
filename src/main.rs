bdd883974e8614ba27b9a41ed6753feaf7eeec4a
Felix Haller <felix@haller.family>
2018-02-07 17:55:11 +0100

    Add GA-skeleton and recombination

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,28 @@ use std::fmt;
 use rand::Rng;
 use std::collections::BTreeSet;
 
-fn exit(msg: &str) {
+fn exit(msg: &str, status: i32) {
     eprintln!("{}", msg);
-    std::process::exit(1);
+    std::process::exit(status);
 }
 
 fn is_usize(s: String) -> Result<(), String> {
     match s.parse::<usize>() {
-        Ok(u) => Ok(()),
+        Ok(_) => Ok(()),
         Err(_) => Err("Ist kein Integer-Wert".to_string())
     }
 }
+
+fn is_even_usize(s: String) -> Result<(), String> {
+    match s.parse::<usize>() {
+        Ok(u) =>  match u%2 {
+            0 => Ok(()),
+            _ => Err("Dieser Algorithmus akzeptiert nur gerade Werte".to_string())
+        },
+        Err(_) => Err("Ist kein Integer-Wert".to_string())
+    }
+}
+
 
 fn main() {
     let matches = App::new("btwns")
@@ -55,14 +66,14 @@ fn main() {
             .required(false)
             .validator(is_usize)
             .default_value("1000"))
-        .arg(Arg::with_name("unchanged")
+        .arg(Arg::with_name("maxunchanged")
             .short("u")
-            .value_name("UNCHANGED")
-            .help("Anzahl der max. aufeinanderfolgenden Generationen ohne Veränderung")
+            .value_name("MAXUNCHANGED")
+            .help("Anzahl der max. aufeinanderfolgenden Generationen ohne Verbesserung")
             .takes_value(true)
             .required(false)
             .validator(is_usize)
-            .default_value("100"))
+            .default_value("500"))
         .arg(Arg::with_name("popsize")
             .short("p")
             .long("pop")
@@ -71,30 +82,29 @@ fn main() {
             .takes_value(true)
             .required(false)
             .validator(is_usize)
+            .validator(is_even_usize)
             .default_value("100"))
         .get_matches();
 
-    let mutation = matches.value_of("mutation").unwrap();
+    let mutation = matches.value_of("mutation").expect("Fehler beim Parsen");
     let elements = matches.value_of("elements").unwrap().parse::<usize>().expect("Fehler beim Parsen");
     let triples = matches.value_of("triples").unwrap().parse::<usize>().expect("Fehler beim Parsen");
     let generations = matches.value_of("generations").unwrap().parse::<usize>().expect("Fehler beim Parsen");
-    let unchanged = matches.value_of("unchanged").unwrap().parse::<usize>().expect("Fehler beim Parsen");
+    let maxunchanged = matches.value_of("maxunchanged").unwrap().parse::<usize>().expect("Fehler beim Parsen");
     let popsize = matches.value_of("popsize").unwrap().parse::<usize>().expect("Fehler beim Parsen");
 
-    solve_solvable(elements, triples, generations, unchanged, popsize, mutation);
+    solve_solvable(elements, triples, generations, maxunchanged, popsize, mutation);
 }
 
 struct Individual {
-    id : u32,
-    generation: u32,
+    generation: usize,
     // Das Genome
     genome: Vec<usize>,
-    // Eine umgekehrte Zuordnung um schnell den Index eines Elements zu erfahren
 }
 
     impl fmt::Display for Individual {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            write!(f, "#{}[{}]: {}", self.id, self.generation, format!("{:?}", self.genome))
+            write!(f, "[{}]: {}", self.generation, format!("{:?}", self.genome))
         }
     }
 
@@ -107,10 +117,10 @@ struct Individual {
             rand::thread_rng().shuffle(&mut genome);
 
 
-            Individual{id : 0, generation : 1, genome: genome}
+            Individual{generation : 1, genome: genome}
         }
 
-        fn mutate(&mut self) {
+        fn mutate(&mut self, mutation: &str) {
 
             // vertauschende Mutation
             let a = rand::thread_rng().gen_range(0, self.genome.len());
@@ -124,8 +134,26 @@ struct Individual {
             // verschiebende Mutation (gut geeignet laut Skript)
 
         }
-        fn recombine(&self, other : Individual) {
-            // ORDNUNGSREKOMBINATION (Buch S.29)
+        fn recombine(&self, other : Individual) -> (Individual, Individual) {
+            // ORDNUNGSREKOMBINATION (ohne Klone) (Buch S.29)
+            let new_gen = self.generation + 1;
+            let crosspoint = rand::thread_rng().gen_range(1, self.genome.len()-1);
+
+            let mut child1 = Individual{generation: new_gen, genome: self.genome[0..crosspoint].to_vec()};
+            let mut child2 = Individual{generation: new_gen, genome: other.genome[0..crosspoint].to_vec()};
+
+            for parent_genome in vec![&self.genome, &other.genome] {
+                let mut genome = parent_genome.to_vec(); //TODO: ohne Kopie lösen?
+                while let Some(element) = genome.pop() {
+                    if ! child1.genome.contains(&element) {
+                        child1.genome.push(element);
+                    }
+                    if ! child2.genome.contains(&element) {
+                        child2.genome.push(element);
+                    }
+                }
+            }
+            (child1, child2)
         }
     }
 
@@ -178,7 +206,7 @@ struct BTWNSProblem {
         }
     }
 
-fn solve_solvable(elements: usize, triples: usize, generations: usize, unchanged: usize, popsize: usize, mutation: &str) {
+fn solve_solvable(elements: usize, triples: usize, generations: usize, max_unchanged: usize, popsize: usize, mutation: &str) {
     // Permutation erzeugen
     let solution = Individual::new_random(elements);
     // Regeln erzeugen
@@ -186,25 +214,98 @@ fn solve_solvable(elements: usize, triples: usize, generations: usize, unchanged
     println!("Lösung: {}", solution);
     println!("Regeln: {}", problem);
 
-    // Evoli
-    let mut individuals = Vec::new();
-    for _ in 0..popsize {
-        individuals.push(Individual::new_random(elements));
+    //+++++++++++++++++++++++++++++++++
+    //Zähler für wiederholte beste Werte (Maximum, inGeneration)
+    struct MaxQuality {
+        value: f64,
+        generation: usize,
     }
-    let mut ind = Individual::new_random(elements);
-    let mut max = 0.0;
-    for i in 0..generations {
-        let rating = problem.rate(&ind);
+    let mut max_quality = MaxQuality{value: 0.0, generation: 0};
+
+
+    //Start-Population zufällig erzeugen
+    let mut population = Vec::new();
+    for _ in 0..popsize {
+        population.push(Individual::new_random(elements));
+    }
+
+    // Initiales Bewerten
+    let mut ratings = Vec::new();
+    for individual in population.iter() {
+        //Terminierungsbedingung prüfen
+        let rating = problem.rate(&individual);
         if rating == 1.0 {
-            println!("{}", "=".repeat(80));
-            println!("gefundene Lösung ({} Durchläufe): {}", i, ind);
-            max = rating;
-            break;
+            exit(format!("Lösung: {}", individual).as_str(), 0);
         }
-        ind.mutate();
-        max = max.max(rating);
+        if rating > max_quality.value {
+            max_quality = MaxQuality{value: rating, generation: 0};
+        }
+        ratings.push(rating);
 
     }
-    println!("Max: {}", max);
+    for generation in 1..generations {
+        // Terminierungsbedingungen prüfen
+//        if generation - max_quality.generation > max_unchanged {
+//            exit(format!("Keine Verbesserung seit {} Generationen", max_unchanged).as_str(), 1);
+//        }
+        if max_quality.value == 1.0 {
+            for individual in population.iter() {
+                if problem.rate(&individual) == 1.0 {
+                    exit(format!("Lösung: {}", individual).as_str(), 0);
+                }
+            }
+        }
+        // Paarungsselektion
+        //SKIP
+
+        // Rekombination
+        let mut new_population = Vec::new();
+        rand::thread_rng().shuffle(&mut population);
+        while let Some(individual) = population.pop() {
+            let (child1, child2) = individual.recombine(population.pop().expect("Populationszahl war ungerade!"));
+            new_population.push(child1);
+            new_population.push(child2);
+        }
+        //Eltern werden durch Kinder vollständig ersetzt
+        population = new_population;
+
+        // Mutation der Kinder
+        for individual in population.iter_mut() {
+            individual.mutate(mutation);
+        }
+
+
+        // Bewertung
+        ratings = Vec::new();
+        for individual in population.iter() {
+            let rating = problem.rate(&individual);
+            if rating > max_quality.value {
+                max_quality = MaxQuality{value: rating, generation};
+            }
+            ratings.push(rating);
+        }
+        println!("Generation: {} \t\t max: {}", generation, max_quality.value);
+
+        // Umweltselektion
+    }
+
+
+//OLD STUFF
+
+
+//    let mut max = 0.0;
+//    for i in 0..generations {
+//        let rating = problem.rate(&ind);
+//        if rating == 1.0 {
+//            println!("{}", "=".repeat(80));
+//            println!("gefundene Lösung ({} Durchläufe): {}", i, ind);
+//            max = rating;
+//            break;
+//        }
+//        ind.mutate();
+//        max = max.max(rating);
+//
+//    }
+//    println!("Max: {}", max);
 
 }
